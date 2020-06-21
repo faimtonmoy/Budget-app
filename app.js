744430a652b97c8bd0c832b11cbb9936c7fb0244
FaimTonmoy <bhuiyanfaim@gmail.com>
2020-06-21 22:37:45 +0600

    Update app.js

--- app.js
+++ app.js
@@ -4,17 +4,41 @@ var budgetController = (function(){
 
 
 var UIController = (function(){
+    var DomString ={
+        inputType: '.add__type',
+        inputDesciption :'.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+
+    };
+
+    return{
+        getInput: function(){
+            return{
+                type: document.querySelector(DomString.inputType).value,
+                description: document.querySelector(DomString.inputDesciption).value,
+                value: document.querySelector(DomString.inputValue).value
+            };
+        },
+        getDomstring: function(){
+            return DomString;
+        }
+    };
 
 })();
 
 
-var controller = (function(budgetCtrl, UIctrl){
-
+var controller = (function(budgetCtrl, UICtrl){
+    
+     var DOM = UICtrl.getDomstring();
+    
     var cntrlAddItem= function(){
-
+        
+        var input= UICtrl.getInput();
+        console.log(input);
     }
 
-    document.querySelector('.add__button').addEventListener('click', cntrlAddItem);
+    document.querySelector(DOM.inputButton).addEventListener('click', cntrlAddItem);
 
     document.addEventListener('keypress', function(event){
           if(event.keyCode === 13 || event.which === 13)
