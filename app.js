ce7cbd0875d3c57ca216bc19ddab0abacc441c77
FaimTonmoy <bhuiyanfaim@gmail.com>
2020-06-28 11:21:32 +0600

    Update app.js

--- app.js
+++ app.js
@@ -63,7 +63,9 @@ var UIController = (function(){
         inputType: '.add__type',
         inputDesciption :'.add__description',
         inputValue: '.add__value',
-        inputButton: '.add__btn'
+        inputButton: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
 
     };
 
@@ -75,11 +77,30 @@ var UIController = (function(){
                 value: document.querySelector(DomString.inputValue).value
             };
         },
+        addListItem: function(obj, type){
+            var html, newHtml, element;
+           if(type === 'inc')
+           {
+            element= DomString.incomeContainer;
+            html= '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+           }
+           else if(type === 'exp')
+           {
+               element=DomString.expensesContainer;
+            html='<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+           }
+           newHtml= html.replace('%id%', obj.id);
+           newHtml= newHtml.replace('%description%', obj.description);
+           newHtml= newHtml.replace('%value%', obj.value);
+
+           document.querySelector(element).insertAdjacentHTML('beforeend',newHtml);
+
+           
+        },
         getDomstring: function(){
             return DomString;
         }
     };
-
 })();
 
 
@@ -108,6 +129,8 @@ var controller = (function(budgetCtrl, UICtrl){
       input= UICtrl.getInput();
 
       newItem=  budgetCtrl.addItem(input.type, input.description, input.value);
+        
+      UICtrl.addListItem(newItem, input.type);
         
         
         
