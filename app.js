d7ef0820588759e6551a0c9be0ee14ad0893bf01
FaimTonmoy <bhuiyanfaim@gmail.com>
2020-06-30 00:16:24 +0600

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ var budgetController = (function(){
         this.value= value;
 
     };
+    var calculateTotal = function(type){
+        var sum= 0;
+        data.allItems[type].forEach(function(curr){
+          sum+= curr.value;
+        });
+        data.totals[type]= sum;
+    };
 
     var data = {
 
@@ -25,6 +32,8 @@ var budgetController = (function(){
             exp: 0,
             inc: 0
         }
+        budget: 0,
+        percentage: -1
 
     };
     return{
@@ -50,7 +59,33 @@ var budgetController = (function(){
 
             data.allItems[type].push(newItem);
             return newItem;
-        }
+        },
+        calculateBudget: function()
+        {
+           calculateTotal('exp');
+           calculateTotal('inc');
+
+           data.budget= data.totals.inc- data.totals.exp;
+           if(data.totals.inc>0){
+            
+            data.percentage= Math.round((data.totals.exp/ data.totals.inc)* 100);
+
+           }
+           else
+           {
+               data.percentage= -1;
+           }
+           
+        },
+        getBudget: function()
+        {
+            return{
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            };
+        },
 
     };
 
@@ -132,6 +167,13 @@ var controller = (function(budgetCtrl, UICtrl){
           }
     });
 
+    };
+    
+    var updateBudget = function(){
+         
+        budgetCtrl.calculateBudget();
+
+        var budget = budgetCtrl.getBudget();
     };
     
 
